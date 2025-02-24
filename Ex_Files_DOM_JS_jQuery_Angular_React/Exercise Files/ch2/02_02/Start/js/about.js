14e6e8cd9cea5b4521a7f8d931616f0e7cceda33
unbent <daniel.polan@hotmail.com>
2025-02-24 17:32:01 -0500

    Add event listener to show and hide names buttons

--- Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch2/02_02/Start/js/about.js
+++ Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch2/02_02/Start/js/about.js
@@ -1,3 +1,23 @@
+window.onload = function() {
+    var btnShow = document.getElementById("btnShowNames");
+    var btnHide = document.getElementById("btwHideNames");
+    var result = document.getElementById("team-member-names");
+
+    btnShow.addEventListener("click", function(){
+        var team = document.getElementsByClassName("team-name");
+        var names = "";
+        
+        for (const member of team) {
+            names += member.innerHTML + "<br>";
+        }
+        result.innerHTML = names;
+    });
+
+    btnHide.addEventListener("click", function(){
+        var team = document.getElementsByClassName("team-name");
+        result.innerHTML = "";
+    });
+}
 
 function effectOn(id){
     var member = document.getElementById(id);
