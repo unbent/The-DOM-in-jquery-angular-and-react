57d0b19405e7a2d3595ad2d8d90fe7917efcbf4d
unbent <daniel.polan@hotmail.com>
2025-02-24 18:15:24 -0500

    Create onload function to find all elements by a tag name

--- Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch2/02_04/Start/js/register.js
+++ Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch2/02_04/Start/js/register.js
@@ -1,3 +1,19 @@
+window.onload = function() {
+    var count = 0;
+    var inputElements = document.getElementsByTagName("input");
+
+    for (const element of inputElements) {
+        console.log(`TYPE: ${element.type}`);
+
+        if (element.type == "text") {
+            count++;
+            element.style.border = "2px solid red";
+            element.style.background = "yellow";
+        }
+    }
+
+    console.log(`TOTAL TEXT TYPE: ${count}`);
+}
 
 function validateForm(){
     var error = false;
