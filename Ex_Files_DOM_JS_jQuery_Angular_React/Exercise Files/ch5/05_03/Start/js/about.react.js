785c83fe0bbaa6a7d2c49e1a20d2b46344f58089
unbent <daniel.polan@hotmail.com>
2025-03-12 13:43:38 -0400

    Create React components to simplify page into fewer components

--- Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch5/05_03/Start/js/about.react.js
+++ Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch5/05_03/Start/js/about.react.js
@@ -1,36 +1,88 @@
+// //Standard JS
+// var targetFrankie = document.getElementById("frankie");
+// var countFrankie = 0;
 
-//Standard JS
-var targetFrankie = document.getElementById("frankie");
-var countFrankie = 0;
+// function countfrankie(){
+//     var txt  = "<p>Counter: " + countFrankie++ + "</p>";
+//     txt += "<p>HR Department</p>";
+//     targetFrankie.innerHTML = txt;
+// }
 
-function countfrankie(){
-    var txt  = "<p>Counter: " + countFrankie++ + "</p>";
-    txt += "<p>HR Department</p>";
-    targetFrankie.innerHTML = txt;
-}
+// setInterval(countfrankie,1000);
 
-setInterval(countfrankie,1000);
+// //ReactJS
 
-//ReactJS
+// var targetDon = document.getElementById("don");
+// var countDon = 0;
 
-var targetDon = document.getElementById("don");
-var countDon = 0;
+// function countdon(){
+//     ReactDOM.render(
+//         <div>
+//             <p>Counter: {countDon++}</p>
+//             <p>IT Department</p>
+//         </div>
+//         ,
+//         targetDon
+//     );
+// }
 
-function countdon(){
-    ReactDOM.render(
+// setInterval(countdon,1000);
+
+var targetTeam = document.getElementById("team");
+
+var TeamMember = React.createClass({
+    render: function() {
+        return (
+            <div className="team-member">
+                <img id={this.props.id} className="profile" src={`images/${this.props.id}.png`}/>
+                <p className="team-name">{this.props.name}</p>
+                <p className="team-title">{this.props.title}</p>
+            </div>
+        )
+    }
+});
+
+var teamMembers = [
+    {
+        id: 'frankiesmith',
+        name: 'Frankie Smith',
+        title: 'Sponsor'
+    },
+    {
+        id: 'donlee',
+        name: 'Don Lee',
+        title: 'Developer'
+    },
+    {
+        id: 'nickgreen',
+        name: 'Nick Green',
+        title: 'Team Lead'
+    },
+    {
+        id: 'ninjablack',
+        name: 'Ninja Black',
+        title: 'Project Manager'
+    },
+];
+
+var TeamList = React.createClass({
+    render: () => (
         <div>
-            <p>Counter: {countDon++}</p>
-            <p>IT Department</p>
+            {
+                teamMembers.map((member) => (
+                        <TeamMember 
+                            id={member.id}
+                            name={member.name}
+                            title={member.title}/>
+                    )
+                )
+            }
         </div>
-        ,
-        targetDon
-    );
-}
+    )
+})
 
-setInterval(countdon,1000);
-
-
-
-
-
-
+ReactDOM.render(
+    <TeamList />
+    ,
+    targetTeam
+);
